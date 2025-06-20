b8720ff35497b150116f6426d51064edfa097536
Claudio Raphael Cabral Teixeira <crteixeira@topaz.corp>
2025-06-20 11:31:57 -0300

    ajuste final.

--- src/service/Service.py
+++ src/service/Service.py
@@ -6,13 +6,17 @@ from src.models.indicadoresModel import Indicadores
 
 class Service:
     @classmethod
-    def listar(cls, filtro_indicadores: Indicadores):
+    def listar(cls, filtro_indicadores: Indicadores):  # Recebe um objeto Indicadores completo
+        # Sua query SQL COM o INNER JOIN e o filtro WHERE pelo CodigoMoeda
         query = """
-           SELECT c.dataCotacao, c.valorCotacao
-           FROM cotacao c
-           WHERE c.CodigoMoeda = ?;
-           """
-        params = (filtro_indicadores.codigoMoeda,)
+            SELECT c.dataCotacao, c.valorCotacao
+            FROM indicadores i 
+            INNER JOIN cotacao c ON i.CodigoMoeda = c.CodigoMoeda
+            WHERE i.CodigoMoeda = ?; -- O filtro é aplicado na coluna CodigoMoeda que participa do JOIN
+            """
+
+        # O parâmetro para a query é uma tupla com o codigoMoeda do objeto Indicadores
+        params = (filtro_indicadores.codigoMoeda,)  # Tupla de um elemento, sempre!
 
         lista = Databases.fetch_all(query, params)
 
